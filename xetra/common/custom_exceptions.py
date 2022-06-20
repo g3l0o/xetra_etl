63337fcd77b1b81a3209b1cf2664146613726dad
Rogelio Rivera Meléndez <rogelio.rivera.isc@gmail.com>
2022-06-20 17:36:18 -0500

    feat: s3 implementation complete

--- xetra/common/custom_exceptions.py
+++ xetra/common/custom_exceptions.py
@@ -1,1 +1,8 @@
 """Custom Exceptions"""
+
+class WrongFormatException(Exception):
+    """
+    WrongFormatException class
+
+    Exception that can be raised when the format type given as a parameter is not supported
+    """
